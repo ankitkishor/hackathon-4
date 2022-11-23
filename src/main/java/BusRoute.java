d0faf9ea21f6edf16fb284e6e2b0db70be20bbda
ankit <ankit.kishorelv@gmail.com>
2022-11-23 09:31:26 +0530

    created interface class

---
+++ src/main/java/BusRoute.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusRoute {
+
+//    public static void main(String[] args) {
+//        System.out.println("BusRoute");
+//    }
+
+
+
+    }
